78e097ec33e4c78159a0c2be1ad1d8f2c4137d26
ron <rinbar70@gmail.com>
2020-10-18 20:38:28 +0300

    Lay out the form and the list.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo';
+  items = [
+    { who: 'ron', what: 'One', when: '2020-10-31' },
+    { who: 'ron', what: 'Two', when: '2020-10-31' },
+    { who: 'ron', what: 'Three', when: '2020-10-31' },
+    { who: 'ron', what: 'Four', when: '2020-10-31' },
+    { who: 'ron', what: 'Five', when: '2020-10-31' },
+    { who: 'ron', what: 'Six', when: '2020-10-31' },
+  ];
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-   AppRoutingModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatTableModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
